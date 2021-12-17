c346b1b28362ac9a619d2d87cfe00bfce8e563f0
Exisy <72301308+Exisy@users.noreply.github.com>
2021-12-17 13:15:25 +0330

    fix trcrt

--- modules/traceroute.py
+++ modules/traceroute.py
@@ -1,6 +1,7 @@
 import subprocess
 import platform
 import sys
+import os
 from colorama import Fore 
 def start():
     try:
@@ -8,13 +9,13 @@ def start():
         print(Fore.CYAN+' [!] '+Fore.GREEN+' Please Enter Target IP/Domain. ')
         target=input(Fore.RED+' [~] : ')
         print(Fore.GREEN+'')
-        os=platform.uname()
-        if os[0].lower()=='windows':
+        os1=platform.uname()
+        if os1[0].lower()=='windows':
             subprocess.call('tracert '+target)
-        elif os[0].lower()=='linux':
-            subprocess.call('traceroute '+target)
+        elif os1[0].lower()=='linux':
+            os.system('traceroute '+target)
         else:
-            subprocess.call('traceroute '+target)
+            os.system('traceroute '+target)
     except:
         pass
     try:
